0a97fb6e9970b7acf353c146186340c50d73a537
den-pe <73271460+den-pe@users.noreply.github.com>
2020-10-22 00:25:56 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCX7JV3CKCMT2fWyLNOReFzbk42UZEAFEo",
+    authDomain: "fir-bbf16.firebaseapp.com",
+    databaseURL: "https://fir-bbf16.firebaseio.com",
+    projectId: "fir-bbf16",
+    storageBucket: "fir-bbf16.appspot.com",
+    messagingSenderId: "610507909407",
+    appId: "1:610507909407:web:dc8fc41ed4cba5373ea82a",
+    measurementId: "G-5P2GV71GJ7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ 
---
+++ index.js
@@ -0,0 +1,166 @@
+//llamada a la bsae
+var db = firebase.database();
+
+var update= document.getElementById('update');
+update.disable=true;
+
+function value(request){
+    return  document.getElementById(request).value;
+}
+function asignation(request,response){
+    return document.getElementById(request).value=response;
+}
+function printHTML(request,response){
+    return document.getElementById(request).innerHTML+=response;
+}
+function inHTML(request,response){
+    return document.getElementById(request).innerHTML=response;
+}
+//insertamos en la base
+function insertTask(matricula, nombre,edad,promedio,sexo){
+    db.ref('task/').push({
+        matricula:matricula, nombre:nombre,edad:edad,promedio:promedio,sexo:sexo
+    });
+}
+
+function onClickInsert(){
+    var matricula =value("matricula");
+    var nombre =value("nombre");
+    var edad=value("edad");
+    var promedio=value("promedio");
+    var sexo=value("sexo");
+    if(sexo=="mujer"){
+        inHTML("tablaM","");
+        insertTask( matricula, nombre,edad,promedio,sexo);
+        asignation("matricula","");
+        asignation("nombre","");
+        asignation("edad","");
+        asignation("promedio","");
+        asignation("sexo","");
+        alert("Guardado");
+    }else if(sexo=="hombre"){        
+        inHTML("tablaH","");
+        insertTask( matricula, nombre,edad,promedio,sexo);
+        asignation("matricula","");
+        asignation("nombre","");
+        asignation("edad","");
+        asignation("promedio","");
+        asignation("sexo","");
+        alert("Guardado");
+    }
+    
+}
+
+function updateTask(matricula,nombre,edad,promedio,sexo,key){
+    db.ref('task/'+key).update({
+        matricula:matricula, nombre:nombre,edad:edad,promedio:promedio,sexo:sexo
+    });
+}
+
+
+
+function onClickUpdate(){
+    var matricula= value("maEdit");
+    var nombre= value("nombreEdit");
+    var edad= value("edadEdit");
+    var promedio= value("promEdit");
+    var sexo= value("sexEdit");
+    var key= value("key");
+
+    if(sexo=="mujer"){        
+        inHTML("tablaM","");
+        updateTask(matricula,nombre,edad,promedio,sexo,key);
+        inHTML("editData","");
+        alert("Modificado");
+        update.disable=true;
+    }
+    if(sexo=="hombre"){        
+        inHTML("tablaH","");
+        updateTask(matricula,nombre,edad,promedio,sexo,key);
+        inHTML("editData","");
+        alert("Modificado");
+        update.disable=true;
+    }
+}
+
+
+function removeTask(key){   
+    if(confirm("Seguro de eliminar?")){
+        inHTML("tablaH","");
+        inHTML("tablaM","");
+        db.ref('task/'+key).remove();
+    }
+}
+
+
+function table(matricula,nombre,edad,promedio,sexo,key){
+    return '<tr><td>'+matricula+'</td><td>'+nombre+'</td><td>'+edad+'</td>'+'</td><td>'+promedio+'</td>'+'</td><td>'+sexo+'</td>'+'</td>'+
+    '<td><a href="#" onclick="viewDataUpdate(\''+matricula+'\',\''+nombre+'\',\''+edad+'\',\''+promedio+'\',\''+sexo+'\',\''+key+'\')">'+
+    '<i class="fas fa-edit blue icon-lg"></i></a></td>'+
+    '<td><a href="#" onclick="removeTask(\''+key+'\')">'+
+     '<i class="fas fa-trash-alt red icon-lg"></i></a></td></tr>'; 
+
+}
+function viewDataUpdate(matricula,nombre,edad,promedio,sexo,key){
+    
+    var response = '<div class="form-group"><input type="hidden" value='+key+' id="key">' +
+    '<input type="text" id="maEdit" class="form-control" placeholder="matricula" value='+matricula+'>'+
+    '<textarea placeholder="DescriptionEdit" class="form-control" id="nombreEdit">'+nombre+'</textarea>'+
+    '<textarea placeholder="no me manda la edad" class="form-control" id="edadEdit">'+edad+'</textarea>'+
+    '<textarea placeholder="no me manda la promedio" class="form-control" id="promEdit">'+promedio+'</textarea>'+
+    '<textarea placeholder="no me manda la sexo" class="form-control" id="sexEdit">'+sexo+'</textarea>'+
+    '</div>';
+   
+    inHTML('editData',response);
+    update.disabled = false;
+}
+
+var reference = db.ref('task/');    
+reference.on('value',function(datas){
+    var data = datas.val();
+    $.each(data, function(nodo, value) {
+            var sendData = table(value.matricula,value.nombre,value.edad,value.promedio,value.sexo,nodo);
+            if(value.sexo=="mujer"){                                
+                printHTML('tablaM',sendData);                
+            }else if(value.sexo=="hombre"){
+                printHTML('tablaH',sendData);
+            }
+    });       
+});
+function busca(){
+    var buscar= value("buscar");
+    var reference= db.ref('task');
+    reference.on('value',function(datas){
+        var data=datas.val();
+        inHTML("tablaM","");
+        $.each(data,function(nodo,value){
+            var senData=table(value.matricula,value.nombre,value.edad,value.promedio,value.sexo,nodo);            
+                if(buscar==""){                   
+                        printHTML('tablaM',senData);                     
+                }else if((value.matricula==buscar)){
+                    console.log(""+value.matricula+""+""+value.nombre+""+value.edad+""+""+value.promedio+""+""+value.sexo);
+                        printHTML('tablaM',senData);                     
+                }            
+        });
+    });
+}   
+function buscar(){
+    var busca= value("busca");
+    var reference= db.ref('task');
+    reference.on('value',function(datas){
+        var data=datas.val();
+        inHTML("tablaH","");
+        $.each(data,function(nodo,value){
+            var senData=table(value.matricula,value.nombre,value.edad,value.promedio,value.sexo,nodo);            
+                if(busca==""){                   
+                        printHTML('tablaH',senData);                     
+                }else if((value.matricula==busca)){
+                        printHTML('tablaH',senData);                     
+                }            
+        });
+    });
+}
+
+
+
+
